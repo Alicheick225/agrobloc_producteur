6dc64853a8a9b65a7963fce9a3fa9a4ad15e6321
Ali Ouattara <alicheick@ODA16.local>
2025-06-20 11:11:03 +0000

    Cors Activation

--- main.go
+++ main.go
@@ -6,12 +6,26 @@ import (
 	"agrobloc_producteur/routes"
 
 	"github.com/gin-gonic/gin"
+
+	"github.com/gin-contrib/cors"
+
+	"time"
 )
 
 func main() {
 	config.InitDB()
 
 	router := gin.Default()
+
+	// 🔓 Configuration CORS
+	router.Use(cors.New(cors.Config{
+		AllowOrigins:     []string{"http://192.168.252.15:4200"}, // Remplace * par ton front en prod
+		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE", "OPTIONS"},
+		AllowHeaders:     []string{"Origin", "Content-Type", "Authorization"},
+		ExposeHeaders:    []string{"Content-Length"},
+		AllowCredentials: true,
+		MaxAge:           12 * time.Hour,
+	}))
 
 	// Très important : appel de RegisterRoutes
 	routes.RegisterRoutes(router)
